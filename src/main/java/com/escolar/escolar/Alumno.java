2f739d2dc3ec192544d700d4f4b93d08d939f7e9
Gonzalo Anuar <gonzalo.anuar13@outlook.com>
2023-04-18 20:03:18 -0600

    Se agregaron los metodos get y set, junto con el constructor

--- src/main/java/com/escolar/escolar/Alumno.java
+++ src/main/java/com/escolar/escolar/Alumno.java
@@ -1,7 +1,33 @@
 package com.escolar.escolar;
 
+import java.util.ArrayList;
+
 public class Alumno {
     private int numControl;
     private String nombre;
 
+    static ArrayList<Alumno> alumnos = new ArrayList<>();
+    public Alumno(int numControl, String nombre){
+        this.numControl = numControl;
+        this.nombre = nombre;
+    }
+
+    public static void altaAlumno(){
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumControl() {
+        return numControl;
+    }
+    public void setNumControl(int numControl) {
+        this.numControl = numControl;
+    }
 }
