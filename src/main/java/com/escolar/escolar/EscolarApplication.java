7859ab34fb8fef41020d8f364beaeb73b2fc1fa7
OmarContrerasSosa <omaracs12@gmail.com>
2023-04-18 18:49:05 -0600

    Añado lo referente a la clase Materia

--- src/main/java/com/escolar/escolar/EscolarApplication.java
+++ src/main/java/com/escolar/escolar/EscolarApplication.java
@@ -8,6 +8,10 @@ public class EscolarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EscolarApplication.class, args);
+		/*Materia.altaMaterias(1,"Software");
+		Materia.altaMaterias(2,"Cálculo Integral");
+		Materia.altaMaterias(3,"Desarrollo Sustentable");
+		Materia.mostrarMaterias();*/
 	}
 
 }
